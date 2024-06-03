18b76b10a9d5f83f81f8c905b2aaf5c2e1d1cf57
AdoonayR <162867753+AdoonayR@users.noreply.github.com>
2024-06-02 23:18:23 -0700

    Se lanza la version funcional sincronizada a la base de datos.

---
+++ Migrations/20240602070945_AddResultColumnToQuimicos.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuditoriaQuimicos.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddResultColumnToQuimicos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Result",
+                table: "Quimicos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Result",
+                table: "Quimicos");
+        }
+    }
+}
